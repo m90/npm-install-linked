262d774e09a1fbc9d2357a0817c22d7a6c025e47
Frederik Ring <frederik.ring@gmail.com>
2021-09-18 18:54:07 +0200

    add initial content

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # npm-install-linked
 Install dependencies of linked packages when using npm 7
+
+## What is this good for?
+
+npm 7 [stopped installing transient dependencies][issue] for local dependencies referenced through the `file:` scheme.
+For many projects using pre-workspaces monorepos, this prevents migrating to npm 7 as they rely on the behavior of npm 6.
+This tool is a band-aid to emulate the previous behavior while still being able to use npm 7.
+
+[issue]: https://github.com/npm/cli/issues/2339
+
+## Usage
+
+Install the package as a dev dependency:
+
+```
+npm i npm-install-linked -D
+```
+
+and put it in your `package.json`s `preinstall` script:
+
+```json
+{
+  "scripts": {
+    "preinstall": "npm-install-linked"
+  }
+}
+```
+
+## Options
+
+Two options can be passed to the CLI tool:
+
+### `--force`, `-F`
+
+By default, the command will check for the npm version in use and will do nothing if npm is at version 6 or lower.
+Pass `--force` to override this behavior and forcefully install dependencies in all versions of npm.
+
+### `--walk`, `-W`
+
+By default, the command will only install dependencies for packages referenced usin the `file:` scheme one level deep.
+When passing `--walk` the tool will also install dependencies for such packages referenced by your dependencies.
---
+++ cmd.js
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const cp = require('child_process')
+
+const argv = require('minimist')(process.argv.slice(2), {
+  alias: {
+    walk: 'T',
+    force: 'F'
+  },
+  default: {
+    walk: false,
+    force: false
+  }
+})
+
+void (async () => {
+  if (!argv.force) {
+    const npmVersion = await checkNpmVersion()
+    if (parseInt(npmVersion, 10) < 7) {
+      return `npm version in use is ${npmVersion}, no need to install nested dependencies`
+    }
+  }
+
+  let fileDeps = walkFileDeps(process.cwd(), argv.walk)
+  fileDeps = fileDeps.sort((a, b) => a.depth > b.depth ? -1 : 1)
+  for (const dep of fileDeps) {
+    if (dep.location.indexOf(process.cwd()) === 0) {
+      continue
+    }
+    await install(dep.location)
+  }
+  return `Installed transient dependencies for ${fileDeps.length} "file:" package(s)`
+})()
+  .then((result) => {
+    console.log('install-file-deps: %s', result)
+    process.exit(0)
+  })
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
+
+function walkFileDeps (root, walk, depth = 0) {
+  const pkg = require(path.resolve(root, './package.json'))
+  const deps = Object.assign({}, pkg.dependencies, pkg.devDependencies)
+  let fileDeps = []
+  for (const key in deps) {
+    if (/^file:/.test(deps[key])) {
+      const location = path.resolve(root, deps[key].replace(/^file:/, ''))
+      fileDeps.push({ location, depth })
+      if (walk) {
+        fileDeps = [...fileDeps, ...walkFileDeps(location, walk, depth + 1)]
+      }
+    }
+  }
+  return fileDeps
+}
+
+function install (root) {
+  return new Promise(function (resolve, reject) {
+    const npm = cp.exec('npm install', {
+      cwd: root
+    })
+    npm.on('exit', function (code) {
+      if (code) {
+        reject(new Error(`Installing dependencies for "${root}" failed with exit code ${code}`))
+        return
+      }
+      resolve()
+    })
+  })
+}
+
+function checkNpmVersion () {
+  return new Promise(function (resolve, reject) {
+    const npm = cp.exec('npm -v')
+    let buf = ''
+    npm.stdout.on('data', function (data) {
+      buf += data
+    })
+    npm.on('exit', function (code) {
+      if (code) {
+        reject(new Error(`Determining npm version in use failed with exit code ${code}`))
+        return
+      }
+      resolve(buf.trim())
+    })
+  })
+}
